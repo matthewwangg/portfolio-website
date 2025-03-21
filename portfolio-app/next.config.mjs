6d180861a619ed17784334f28562d2e5f33cfa30
Matt Wang <matthewianwang@gmail.com>
2025-03-20 17:45:41 -0700

    Updated config for new deployment

--- portfolio-app/next.config.mjs
+++ portfolio-app/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/portfolio-website",
-    assetPrefix: "/portfolio-website/",
     trailingSlash: true,
 };
 
